b26c4f17a2f72a4be65a095e586aa0596bd95d01
rpdecks <rpdecks@gmail.com>
2020-04-12 22:56:58 -0400

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,8 +1,13 @@
 class Show < ActiveRecord::Base
     has_many :actors, through: :characters
     has_many :characters
+    belongs_to :network
 
     def actors_list
-        binding.pry
+        list = []
+        self.characters.each do |c|
+            list << c.actor[:first_name] + " " + c.actor[:last_name]
+        end
+        list
     end
 end
