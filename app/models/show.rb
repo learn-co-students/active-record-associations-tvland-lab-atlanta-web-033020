746d44e058438e349343fa6e848357bb8bd4d175
rpdecks <rpdecks@gmail.com>
2020-04-12 22:40:24 -0400

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,8 @@
 class Show < ActiveRecord::Base
-  
+    has_many :actors, through: :characters
+    has_many :characters
+
+    def actors_list
+        binding.pry
+    end
 end
---
+++ db/migrate/20200413010311_add_catchphrase_columnn_to_characters.rb
@@ -0,0 +1,5 @@
+class AddCatchphraseColumnnToCharacters < ActiveRecord::Migration[6.0]
+  def change
+    add_column :characters, :catchphrase, :string 
+  end
+end
